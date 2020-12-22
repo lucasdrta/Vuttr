9d207edad47755b7ee005e70cd7867bbba4270d0
Lucas Duarte <lucasdrtaraujo@gmail.com>
2020-12-22 11:19:58 -0300

    Create a setup for Express

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+const app = express();
+
+export class ServerSetup {
+  constructor(private port = 3000) {
+    this.port = port;
+  }
+
+  public async init(): Promise<void> {
+    this.SetupExpress();
+  }
+
+  private SetupExpress(): void {
+    app.use(express.json());
+    app.use('/', (req, res) => {
+      res.send('Hello');
+    });
+  }
+
+  public start(): void {
+    app.listen(this.port, () => {
+      console.info(`Server running at http://localhost:${this.port}`);
+    });
+  }
+}
