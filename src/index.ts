c3f0db12a48e4ec8925abf08c246620bb0dac7ce
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-01-06 17:27:57 -0300

    Add env variable to port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { ServerSetup } from './server';
+import config from 'config';
 
 (async () => {
-  const server = new ServerSetup();
+  const server = new ServerSetup(config.get('App.port'));
   await server.init();
   server.start();
 })();
