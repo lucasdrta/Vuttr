e25c5b85bd060614c0a294810940a70c5f1cd072
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-01-06 17:28:36 -0300

    Change create method

--- src/controllers/tools.ts
+++ src/controllers/tools.ts
@@ -4,8 +4,9 @@ import { Tool } from '../models/tools';
 export class ToolsController {
   public async create(req: Request, res: Response): Promise<void> {
     try {
-      const tool = await Tool.create(req.body);
-      res.status(201).send(tool);
+      const tool = new Tool(req.body);
+      const result = await tool.save();
+      res.status(201).send(result);
     } catch (error) {
       res.status(400).send({
         code: 400,
