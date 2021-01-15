7ea7edbb6de63bd86b903e943e2023bda91c51ad
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-01-15 17:22:15 -0300

    Add method to list tools

--- src/controllers/tools.ts
+++ src/controllers/tools.ts
@@ -2,6 +2,18 @@ import { Request, Response } from 'express';
 import { Tool } from '../models/tools';
 
 export class ToolsController {
+  public async list(req: Request, res: Response): Promise<void> {
+    try {
+      const tools = await Tool.find();
+      res.status(200).send(tools);
+    } catch (error) {
+      res.status(400).send({
+        code: 400,
+        message: error.message,
+      });
+    }
+  }
+
   public async create(req: Request, res: Response): Promise<void> {
     try {
       const tool = new Tool(req.body);
