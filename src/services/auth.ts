f96b70264a415e2aff4d946dd6024597463875c5
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-01-18 14:07:47 -0300

    Add generate and decode token methods

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,11 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import { IUser } from '../models/user';
+
+export interface DecodedUser extends Omit<IUser, '_id'> {
+  id: string;
+}
 
 export default class AuthService {
   public static async hashPassword(
@@ -13,5 +20,15 @@ export default class AuthService {
     hashedPassword: string
   ): Promise<boolean> {
     return await bcrypt.compare(password, hashedPassword);
+  }
+
+  public static generateToken(payload: Record<string, unknown>): string {
+    return jwt.sign(payload, 'secret-key', {
+      expiresIn: 20000000,
+    });
+  }
+
+  public static decodeToken(token: string): DecodedUser {
+    return jwt.verify(token, 'secret-key') as DecodedUser;
   }
 }
