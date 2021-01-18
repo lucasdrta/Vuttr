2a139dddb370d7db78b8457ebbace28165c6901b
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-01-18 11:36:45 -0300

    Add user route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import tools from './tools';
+import users from './users';
 
 const routes = Router();
 
 routes.use('/tools', tools);
+routes.use('/users', users);
 
 export default routes;
