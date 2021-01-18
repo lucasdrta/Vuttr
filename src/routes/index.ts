ef60e19cf864cf0ad8f59b3466e35cbc72666c0c
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-01-18 14:16:46 -0300

    Add auth middleware to tools route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import { authMiddleware } from '../middlewares/auth';
 import tools from './tools';
 import users from './users';
 
 const routes = Router();
 
-routes.use('/tools', tools);
+routes.use('/tools', authMiddleware, tools);
 routes.use('/users', users);
 
 export default routes;
